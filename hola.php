3ce6db19e122d58d8aeeeb6b7b3fd08a97fb4617
JesicaAre <jesicaarevalo16@gmail.com>
2022-07-23 16:57:24 -0400

    Hola otra vez

--- hola.php
+++ hola.php
@@ -3,15 +3,18 @@
 function saludo($nombre){
     $saludo="Hola ".$nombre;
 
-    echo $saludo."<br>";
+    echo $saludo.' ';
 }
 
 
 
-$alumnos=array("Jesica","Armando","Sebastian","Jesus","Catherine","Ramses");
+$alumnos=array("Jesica"=>"Alumna","Armando"=>"Alumno","Sebastian"=>"Alumno"
+,"Jesus"=>"Alumno","Catherine"=>"Alumna","Ramses"=>"Alumno","Abelardo"=>"Profesor");
 
-foreach ($alumnos as $alumno) {
-    saludo($alumno);
+foreach ($alumnos as $alumno => $cargo) {
+    saludo($cargo);
+    echo "<b>".$alumno."!!</b>";
+    echo "<br>";
 }
 
 ?>
